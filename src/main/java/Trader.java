bb1945d5c823d8e67d6f143a45dff08701aa29dc
yuanjiatan <jiatan.yuan@mail.utoronto.ca>
2022-10-09 19:10:04 -0400

    4

--- src/main/java/Trader.java
+++ src/main/java/Trader.java
@@ -39,7 +39,11 @@ public class Trader<T> {
      *       representing the Trader's money. Give the Trader
      *       empty ArrayLists for their inventory and wishlist.
      */
-
+    public Trader(int money){
+        this.inventory = new ArrayList<T>();
+        this.wishlist = new ArrayList<T>();
+        this.money = money;
+    }
 
 
 
@@ -47,7 +51,9 @@ public class Trader<T> {
     /* TODO: Implement the method addToWishlist that takes an
      *       object of type T and adds it to this Trader's wishlist.
      */
-
+public void addToWishlist(T item){
+    this.wishlist.add(item);
+}
 
 
 
@@ -59,7 +65,12 @@ public class Trader<T> {
      *
      *       We will call this in exchangeMoney().
      */
-
+    public int getSellingPrice(T object){
+        if (object instanceof Tradable){
+            return (((Tradable) object).getPrice());
+        }
+        return Tradable.MISSING_PRICE;
+    }
 
 
 
